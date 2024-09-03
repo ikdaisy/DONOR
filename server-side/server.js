89b38b9ebb7c680678d818fec1e452cf31af99b4
safa <safakallianthodi6@gmail.com>
2024-09-03 13:30:08 +0530

    Added

---
+++ server-side/server.js
@@ -0,0 +1,82 @@
+const http = require ("http")
+const fs = require ("fs")
+const url = require ("url")
+// import querystring module
+const queryString = require("querystring")
+//import the mongodb module 
+const {MongoClient}= require("mongodb")
+// connect the mongodb
+const client=  new MongoClient("mongodb://127.0.0.1:27017/")
+
+const app= http.createServer((req,res)=>{
+    //create database
+    const db = client.db("Donor")
+    //create collection
+    const collection =db.collection("donors")
+    //convert the url into object
+    const path = url.parse(req.url)
+     console.log(path);
+     
+     
+    if(path.pathname=="/"){
+        res.writeHead(200,{"Content-Type":"text/html"})
+        res.end(fs.readFileSync("../client-side/index.html"))
+    }
+    else if(path.pathname=="/client-side/js/custom.js"){
+        res.writeHead(200,{"Content-Type":"text/js"})
+        res.end(fs.readFileSync("../client-side/js/custom.js"))
+    }
+    else if(path.pathname=="/css/addDonor.css"){
+        res.writeHead(200,{"Content-Type":"text/css"})
+        res.end(fs.readFileSync("../client-side/css/addDonor.css"))
+    }
+    else if(path.pathname=="/css/index.css"){
+        res.writeHead(200,{"Content-Type":"text/css"})
+        res.end(fs.readFileSync("../client-side/css/index.css"))
+    }
+    else if(path.pathname=="/add"){
+        res.writeHead(200,{"Content-Type":"text/html"})
+        res.end(fs.readFileSync("../client-side/pages/AddDonor.html"))
+    }
+    console.log(req.method);
+
+    if(path.pathname=="/submit" && req.method=="POST" ){
+        let body=""
+        req.on("data",(chunk)=>{
+            
+            console.log(chunk);
+            body+=chunk.toString()
+            console.log(body);
+            
+            
+        });
+        req.on("end",async()=>{
+            if(body!=null){
+                //convert the queryForm data into object 
+                const formData= queryString.parse(body)
+                console.log(formData);
+                // below statement returns a promise so we use .then()
+                collection.insertOne(formData).then(()=>{
+                    console.log("success");
+                    
+                })
+                .catch((error)=>{
+                    console.log(error);
+                    
+                });
+
+                
+            }
+        })
+        // redirect the page into homepage after submitting
+        res.writeHead(200,{"Content-Type":"text/html"})
+        res.end(fs.readFileSync("../client-side/index.html"))
+    }
+
+    
+ 
+    
+     
+})
+
+app.listen(3001);
