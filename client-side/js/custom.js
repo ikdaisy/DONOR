63c07a6767fb03fb7682f25af768f4adbe84b3ac
safa <safakallianthodi6@gmail.com>
2024-09-04 21:05:49 +0530

    Added

--- client-side/js/custom.js
+++ client-side/js/custom.js
@@ -7,12 +7,12 @@ async function fetchData() {
     data.map((donor)=>{
         // console.log(donor);
         
-        str+=`<div> <input type="text" name="name" placeholder="Name" value=${donor.name} id="name">
-            <input type="text" name="gender" placeholder="Gender" value=${donor.gender} id="gender">
+        str+=`<div> <input type="text" name="name" disabled=true placeholder="Name" value=${donor.name} id="name">
+            <input type="text" name="gender" disabled=true placeholder="Gender" value=${donor.gender} id="gender">
 
-            <input type="text" name="email" placeholder="Email" value=${donor.email} id="email">
-            <input type="text" name="phone" placeholder="Phone Number" value=${donor.phone} id="phone">
-            <input type="text" name="bloodgroup" placeholder="BloodGroup" value=${donor.bloodgroup} id="bloodgroup">
+            <input type="text" name="email" disabled=true placeholder="Email" value=${donor.email} id="email">
+            <input type="text" name="phone" disabled=true placeholder="Phone Number" value=${donor.phone} id="phone">
+            <input type="text" name="bloodgroup" disabled=true placeholder="BloodGroup" value=${donor.bloodgroup} id="bloodgroup">
 
             <button class="edit-btn">EDIT</button>
             <button class="delete-btn">DELETE</button>
